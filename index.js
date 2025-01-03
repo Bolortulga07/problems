a1ff78f70c0f724d30a3984ad0acc64dea01e00d
bolortulga10 <bolortulga07@gmail.com>
2025-01-03 17:17:42 +0800

    haichchuluu

--- index.js
+++
@@ -1,9 +0,0 @@
-game = () => {
-  const random = Math.random();
-
-  return random < 0.5 ? "Heads" : "Tails";
-};
-game();
-window.alert("Tossing the coin.");
-const result = game();
-window.alert(`Result: ${result}`);
